cf43df12001a58262ed538781796d8e0d9d526f7
Leonshacaden <luis.alfredo96@gmail.com>
2022-03-12 11:42:21 -0600

    Correction method showDialogAddCart
    Se implemento async en el metodo y corrección en la validación cuando se selecciona una paca que ya estaba en el listado

--- app_inventory/lib/paca_register.dart
+++ app_inventory/lib/paca_register.dart
@@ -27,10 +27,9 @@ class _PacaRegister extends State<PacaRegister> {
   void initState() {
     // TODO: implement initState
     super.initState();
-    getCategoryPacas();
   }
 
-  void getCategoryPacas() async {
+  Future<bool> getCategoryPacas() async {
     CollectionReference collectionReference =
         FirebaseFirestore.instance.collection('Inventory');
     QuerySnapshot querySnapshot = await collectionReference.get();
@@ -41,6 +40,9 @@ class _PacaRegister extends State<PacaRegister> {
             price: doc.get('price').toDouble(),
             provider: doc.get('provider')));
       }
+      return true;
+    } else {
+      return false;
     }
   }
 
@@ -66,7 +68,8 @@ class _PacaRegister extends State<PacaRegister> {
                 } else {
                   int indexMatch = null;
                   for (int i = 0; i < listPaca.length; i++) {
-                    if (identical(listPaca[i].name, itemPaca.name)) {
+                    print(listPaca[i].name + " == " + itemPaca.name);
+                    if (listPaca[i].name == itemPaca.name) {
                       indexMatch = i;
                     }
                   }
@@ -103,167 +106,177 @@ class _PacaRegister extends State<PacaRegister> {
     );
   }
 
-  Future<Paca> showDialogAddCart() {
+  Future<dynamic> showDialogAddCart() async {
     _amountPacas = 0;
     _dropdownValue = null;
     _isEnabledFieldPrice = false;
     _priceController = TextEditingController();
     Paca itemPaca = Paca();
-    return showDialog(
-      context: context,
-      builder: (context) => StatefulBuilder(
-        builder: (context, setState) => AlertDialog(
-          title: const Text('Añadir Paca al listado'),
-          content: Column(
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              //DropDownMenu
-              AnimatedContainer(
-                duration: Duration(seconds: 2),
-                curve: Curves.fastOutSlowIn,
-                padding: EdgeInsets.only(left: 5, right: 5),
-                decoration: _boxDecorationAlert,
-                child: DropdownButton(
-                    hint: Text('Seleccione una opción'),
-                    value: _dropdownValue,
-                    items: categoryPacas.map((tipoPaca) {
-                      return DropdownMenuItem(
-                          value: tipoPaca, child: Text(tipoPaca.name));
-                    }).toList(),
-                    onChanged: (newValue) {
-                      setState(() {
-                        _dropdownValue = newValue;
-                        _priceController.text = _dropdownValue.price.toString();
-                        _isEnabledFieldPrice = true;
-                        _boxDecorationAlert = null;
-                        _amountPacas = 1;
-                      });
-                    }),
-              ),
-              //TextForm del precio y botones de cantidad
-              Row(
-                crossAxisAlignment: CrossAxisAlignment.center,
-                mainAxisAlignment: MainAxisAlignment.spaceAround,
-                children: [
-                  //TextFormField del precio
-                  Container(
-                    width: 100,
-                    child: TextFormField(
-                      enabled: _isEnabledFieldPrice,
-                      controller: _priceController,
-                      decoration: const InputDecoration(
-                          border: OutlineInputBorder(),
-                          hintText: 'Precio',
-                          labelText: 'Precio'),
-                      keyboardType: TextInputType.number,
-                      inputFormatters: [FilteringTextInputFormatter.digitsOnly],
-                    ),
-                  ),
-                  //Boton de cantidad de Pacas
-                  Container(
-                    width: 120,
-                    height: 40,
-                    clipBehavior: Clip.antiAlias,
-                    decoration: BoxDecoration(
-                      color: Color(0xffefefef),
-                      borderRadius: BorderRadius.all(
-                        Radius.circular(50.0),
+    bool showAlert = await getCategoryPacas();
+    if (showAlert) {
+      return showDialog(
+        context: context,
+        builder: (context) => StatefulBuilder(
+          builder: (context, setState) => AlertDialog(
+            title: const Text('Añadir Paca al listado'),
+            content: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                //DropDownMenu
+                AnimatedContainer(
+                  duration: Duration(seconds: 2),
+                  curve: Curves.fastOutSlowIn,
+                  padding: EdgeInsets.only(left: 5, right: 5),
+                  decoration: _boxDecorationAlert,
+                  child: DropdownButton(
+                      hint: Text('Seleccione una opción'),
+                      value: _dropdownValue,
+                      items: categoryPacas.map((tipoPaca) {
+                        return DropdownMenuItem(
+                            value: tipoPaca, child: Text(tipoPaca.name));
+                      }).toList(),
+                      onChanged: (newValue) {
+                        setState(() {
+                          _dropdownValue = newValue;
+                          _priceController.text =
+                              _dropdownValue.price.toString();
+                          _isEnabledFieldPrice = true;
+                          _boxDecorationAlert = null;
+                          _amountPacas = 1;
+                        });
+                      }),
+                ),
+                //TextForm del precio y botones de cantidad
+                Row(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: [
+                    //TextFormField del precio
+                    Container(
+                      width: 100,
+                      child: TextFormField(
+                        enabled: _isEnabledFieldPrice,
+                        controller: _priceController,
+                        decoration: const InputDecoration(
+                            border: OutlineInputBorder(),
+                            hintText: 'Precio',
+                            labelText: 'Precio'),
+                        keyboardType: TextInputType.number,
+                        inputFormatters: [
+                          FilteringTextInputFormatter.digitsOnly
+                        ],
                       ),
                     ),
-                    margin: EdgeInsets.only(top: 20.0),
-                    padding: EdgeInsets.all(2.0),
-                    child: new Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: <Widget>[
-                        Expanded(
-                          flex: 1,
-                          child: Container(
-                            color: Color(0xffefefef),
-                            child: IconButton(
-                              icon: Icon(Icons.remove),
-                              onPressed: () {
-                                setState(() {
-                                  (_amountPacas > 1)
-                                      ? _amountPacas--
-                                      : print('');
-                                });
-                              },
-                              color: Colors.black,
+                    //Boton de cantidad de Pacas
+                    Container(
+                      width: 120,
+                      height: 40,
+                      clipBehavior: Clip.antiAlias,
+                      decoration: BoxDecoration(
+                        color: Color(0xffefefef),
+                        borderRadius: BorderRadius.all(
+                          Radius.circular(50.0),
+                        ),
+                      ),
+                      margin: EdgeInsets.only(top: 20.0),
+                      padding: EdgeInsets.all(2.0),
+                      child: new Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: <Widget>[
+                          Expanded(
+                            flex: 1,
+                            child: Container(
+                              color: Color(0xffefefef),
+                              child: IconButton(
+                                icon: Icon(Icons.remove),
+                                onPressed: () {
+                                  setState(() {
+                                    (_amountPacas > 1)
+                                        ? _amountPacas--
+                                        : print('');
+                                  });
+                                },
+                                color: Colors.black,
+                              ),
                             ),
                           ),
-                        ),
-                        Expanded(
-                          flex: 1,
-                          child: Container(
-                            color: Colors.white,
-                            child: Center(
-                              child: Text(
-                                '$_amountPacas',
-                                style: new TextStyle(
-                                  fontWeight: FontWeight.bold,
-                                  fontSize: 22.0,
-                                  color: Colors.black,
+                          Expanded(
+                            flex: 1,
+                            child: Container(
+                              color: Colors.white,
+                              child: Center(
+                                child: Text(
+                                  '$_amountPacas',
+                                  style: new TextStyle(
+                                    fontWeight: FontWeight.bold,
+                                    fontSize: 22.0,
+                                    color: Colors.black,
+                                  ),
                                 ),
                               ),
                             ),
                           ),
-                        ),
-                        Expanded(
-                          flex: 1,
-                          child: Container(
-                            color: Color(0xffefefef),
-                            child: IconButton(
-                              icon: Icon(Icons.add),
-                              onPressed: () {
-                                setState(() {
-                                  _amountPacas++;
-                                });
-                              },
-                              color: Colors.black,
+                          Expanded(
+                            flex: 1,
+                            child: Container(
+                              color: Color(0xffefefef),
+                              child: IconButton(
+                                icon: Icon(Icons.add),
+                                onPressed: () {
+                                  setState(() {
+                                    _amountPacas++;
+                                  });
+                                },
+                                color: Colors.black,
+                              ),
                             ),
                           ),
-                        ),
-                      ],
+                        ],
+                      ),
                     ),
-                  ),
-                ],
-              ),
-              //TextoButton para añadir nueva categoría
+                  ],
+                ),
+                //TextoButton para añadir nueva categoría
+                TextButton(
+                  child: Text('Añadir\nCategoria', textAlign: TextAlign.center),
+                  onPressed: () {},
+                ),
+              ],
+            ),
+            actions: <Widget>[
               TextButton(
-                child: Text('Añadir\nCategoria', textAlign: TextAlign.center),
-                onPressed: () {},
+                onPressed: () {
+                  categoryPacas = [];
+                  Navigator.of(context).pop();
+                },
+                child: const Text('Cancel'),
+              ),
+              TextButton(
+                onPressed: () {
+                  if (_dropdownValue == null) {
+                    setState(() {
+                      _boxDecorationAlert = BoxDecoration(
+                          borderRadius: BorderRadius.circular(10),
+                          border: Border.all(
+                              color: Colors.red,
+                              width: 1,
+                              style: BorderStyle.solid));
+                    });
+                  } else {
+                    itemPaca = Paca(
+                        name: _dropdownValue.name,
+                        price: double.parse(_priceController.text),
+                        amount: _amountPacas);
+                    categoryPacas = [];
+                    Navigator.of(context).pop(itemPaca);
+                  }
+                },
+                child: const Text('OK'),
               ),
             ],
           ),
-          actions: <Widget>[
-            TextButton(
-              onPressed: () => Navigator.of(context).pop(),
-              child: const Text('Cancel'),
-            ),
-            TextButton(
-              onPressed: () {
-                if (_dropdownValue == null) {
-                  setState(() {
-                    _boxDecorationAlert = BoxDecoration(
-                        borderRadius: BorderRadius.circular(10),
-                        border: Border.all(
-                            color: Colors.red,
-                            width: 1,
-                            style: BorderStyle.solid));
-                  });
-                } else {
-                  itemPaca = Paca(
-                      name: _dropdownValue.name,
-                      price: _dropdownValue.price,
-                      amount: _amountPacas);
-                  Navigator.of(context).pop(itemPaca);
-                }
-              },
-              child: const Text('OK'),
-            ),
-          ],
         ),
-      ),
-    );
+      );
+    }
   }
 }
