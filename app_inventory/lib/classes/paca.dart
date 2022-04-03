986696621932ed6d12daf7fa9bfa44c813dc12ea
Leonshacaden <luis.alfredo96@gmail.com>
2022-04-02 23:36:02 -0600

    Paca Get & Set
    Las propiedades de la clase se volvieron privadas y se crearon los metodos get y set

--- app_inventory/lib/classes/paca.dart
+++ app_inventory/lib/classes/paca.dart
@@ -1,8 +1,61 @@
 class Paca {
-  String name;
-  double price;
-  int amount;
-  String provider;
+  String _name;
+  String _label;
+  double _price;
+  int _amount;
+  String _provider;
 
-  Paca({this.name, this.price, this.amount, this.provider});
+  Paca({
+    String name,
+    String label,
+    double price,
+    int amount,
+    String provider,
+  }) {
+    this._name = name;
+    this._label = label;
+    this._price = (price) ?? 0;
+    this._amount = (amount) ?? 0;
+    this._provider = provider;
+  }
+
+  String getName() {
+    return this._name;
+  }
+
+  String getLabel() {
+    return this._label;
+  }
+
+  double getPrice() {
+    return this._price;
+  }
+
+  int getAmount() {
+    return this._amount;
+  }
+
+  String getProvider() {
+    return this._provider;
+  }
+
+  void setName(String name) {
+    this._name = name;
+  }
+
+  void setLabel(String label) {
+    this._label = label;
+  }
+
+  void setPrice(double price) {
+    this._price = price;
+  }
+
+  void setAmount(int amount) {
+    this._amount = amount;
+  }
+
+  void setProvider(String provider) {
+    this._provider = provider;
+  }
 }
