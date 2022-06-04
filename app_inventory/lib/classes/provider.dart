200ce12c81b425646e2a15895561c198404123e0
Ednover <ed_leo1411@hotmail.com>
2022-06-04 02:32:56 -0500

    Actualización provider
    Se agregaron los atributos id y balance con sus respectivos métodos get y set.

--- app_inventory/lib/classes/provider.dart
+++ app_inventory/lib/classes/provider.dart
@@ -1,16 +1,26 @@
 class Provider {
+  String _id;
   String _name;
   String _locale;
   int _phone;
+  double _balance;
 
   Provider({
+    String id,
     String name,
     String locale,
     int phone,
+    double balance,
   }) {
+    this._id = id;
     this._name = name;
     this._locale = locale;
     this._phone = (phone) ?? 0;
+    this._balance = (balance) ?? 0;
+  }
+
+  String getId() {
+    return this._id;
   }
 
   String getName() {
@@ -25,6 +35,14 @@ class Provider {
     return this._phone;
   }
 
+  double getBalance() {
+    return this._balance;
+  }
+
+  void setId(String id) {
+    this._id = id;
+  }
+
   void setName(String name) {
     this._name = name;
   }
@@ -35,5 +53,9 @@ class Provider {
 
   void setPhone(int phone) {
     this._phone = phone;
+  }
+
+  void setBalance(double balance) {
+    this._balance = balance;
   }
 }
