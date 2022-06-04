9c7e16c6cf6e08af5498cb996e84f7f97da17383
Ednover <ed_leo1411@hotmail.com>
2022-06-04 02:43:58 -0500

    Actualización available_list
    Cambios menores:
    -Centrado del titulo de appBar.
    -Aplicación de formato al código.

--- app_inventory/lib/home/lists/available_list.dart
+++ app_inventory/lib/home/lists/available_list.dart
@@ -10,7 +10,6 @@ class AvailableList extends StatefulWidget {
 }
 
 class _AvailableList extends State<AvailableList> {
-
   @override
   void initState() {
     super.initState();
@@ -21,40 +20,40 @@ class _AvailableList extends State<AvailableList> {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Pacas vendidas'),
+        centerTitle: true,
       ),
       backgroundColor: Color(0xffefefef),
       body: StreamBuilder(
-        stream: FirebaseFirestore.instance
-            .collection('Inventory')
-            .where('amount', isGreaterThan: 0)
-            .snapshots(),
-        builder:
-            (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
-          if (!snapshot.hasData)
-            return Center(child: CircularProgressIndicator());
-          return ListView.separated(
-            padding: EdgeInsets.only(top: 15, bottom: 15),
-            itemBuilder: (BuildContext context, int index) {
-              return Container(
-                margin: EdgeInsets.only(left: 10, right: 10),
-                child: PacaCard(
-                  paca: new Paca(
-                    name: snapshot.data.docs[index].get('name'),
-                    amount: snapshot.data.docs[index].get('amount').toInt(),
-                    price: snapshot.data.docs[index].get('price').toDouble(),
-                    provider: snapshot.data.docs[index].get('provider'),
+          stream: FirebaseFirestore.instance
+              .collection('Inventory')
+              .where('amount', isGreaterThan: 0)
+              .snapshots(),
+          builder:
+              (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
+            if (!snapshot.hasData)
+              return Center(child: CircularProgressIndicator());
+            return ListView.separated(
+              padding: EdgeInsets.only(top: 15, bottom: 15),
+              itemBuilder: (BuildContext context, int index) {
+                return Container(
+                  margin: EdgeInsets.only(left: 10, right: 10),
+                  child: PacaCard(
+                    paca: new Paca(
+                      name: snapshot.data.docs[index].get('name'),
+                      amount: snapshot.data.docs[index].get('amount').toInt(),
+                      price: snapshot.data.docs[index].get('price').toDouble(),
+                      provider: snapshot.data.docs[index].get('provider'),
+                    ),
                   ),
-                ),
-              );
-            },
-            separatorBuilder: (BuildContext context, int index) => Container(
-              width: double.infinity,
-              height: 10,
-            ),
-            itemCount: snapshot.data.docs.length,
-          );
-        }
-      ),
+                );
+              },
+              separatorBuilder: (BuildContext context, int index) => Container(
+                width: double.infinity,
+                height: 10,
+              ),
+              itemCount: snapshot.data.docs.length,
+            );
+          }),
     );
   }
 }
